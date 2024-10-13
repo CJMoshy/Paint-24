66bda49a1795c97fc61a3023c477918fdec9de23
= <cjmoshy12@gmail.com>
2024-10-13 15:34:34 -0700

    Added a title and a canvas to the screen. (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import "./style.css";
 
 const APP_NAME = "Paint World";
+document.title = APP_NAME;
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+const CANVAS_HEIGHT = 256;
+const CANVAS_WIDTH = 256;
+
+const title = document.createElement('h1')
+title.textContent = 'Paint World'
+
+const canvas = document.createElement('canvas')
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+canvas.className = 'main-canvas';
+
+app.append(title);
+app.append(canvas);
