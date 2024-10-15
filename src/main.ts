49d50e3d5e6915bb69d2fc97a9716e11b883707e
= <cjmoshy12@gmail.com>
2024-10-15 11:05:48 -0700

    Added in an undo/redo system and changed some logic relative to object structure. (step 4 complete)

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const CANVAS_HEIGHT = 256;
 const CANVAS_WIDTH = 256;
 
 const title = document.createElement("h1");
-title.textContent = "Paint World";
+title.textContent = "Paint";
 
 const canvas = document.createElement("canvas");
 canvas.width = CANVAS_WIDTH;
@@ -19,13 +19,24 @@ const clear_button = document.createElement("button");
 clear_button.className = "clear-btn";
 clear_button.textContent = "clear";
 
+const undo_button = document.createElement("button");
+undo_button.className = "clear-btn";
+undo_button.textContent = "undo";
+
+const redo_button = document.createElement("button");
+redo_button.className = "clear-btn";
+redo_button.textContent = "redo";
+
 app.append(title);
 app.append(canvas);
 app.append(clear_button);
+app.append(undo_button);
+app.append(redo_button);
 
 // Simple Drawing Logic
-let line: Point[] = [];
-const lines: Point[][] = [];
+let line: Line = [];
+const lines: Line[] = [];
+const redo_stack: Line[] = [];
 const pen = { active: false, x: 0, y: 0 };
 const ctx = canvas.getContext("2d");
 ctx!.fillStyle = "white";
@@ -46,6 +57,7 @@ const start_line_handler = (e: MouseEvent) => {
 };
 
 const finish_line_handler = () => {
+  if (!pen.active) return;
   lines.push(line);
   line = [];
   pen.active = false;
@@ -81,6 +93,21 @@ const clear_canvas = () => {
   ctx?.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   line.length = 0;
   lines.length = 0;
+  redo_stack.length = 0;
+};
+
+const handle_undo_redo = (undo: boolean) => {
+  console.log("click", lines);
+  if (undo && lines.length === 0) return;
+  if (!undo && redo_stack.length === 0) return;
+  if (undo) {
+    redo_stack.push(lines.pop() as Line);
+  } else {
+    lines.push(redo_stack.pop() as Line);
+  }
+  canvas.dispatchEvent(new Event("drawing-changed"));
 };
 
 clear_button.addEventListener("click", clear_canvas);
+undo_button.addEventListener("click", () => handle_undo_redo(true));
+redo_button.addEventListener("click", () => handle_undo_redo(false));
