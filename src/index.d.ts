c9068d4809ef2f91c4305fec0b63f9fc7de7286e
= <cjmoshy12@gmail.com>
2024-10-19 16:08:34 -0700

    added visual support for drawing widths (step 7 complete)

--- src/index.d.ts
+++ src/index.d.ts
@@ -10,7 +10,14 @@ interface LineCommand {
   execute: () => void;
 }
 
+interface CursorCommand {
+  active: boolean;
+  x: number;
+  y: number;
+  sticker: boolean;
+  execute: (ctx: CanvasRenderingContext2D) => void;
+}
+
 type UndoRedoCommand = (undo: boolean) => void;
 type MarkerCommand = (thin: boolean) => void;
-
-type ActivePen = boolean;
+type MouseMoveCommand = (ctx: CanvasRenderingContext2D) => void;
--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ redo_button.textContent = "redo";
 
 const set_thin_marker_button = document.createElement("button");
 set_thin_marker_button.className = "thin-btn";
-set_thin_marker_button.classList.add('current-marker')
+set_thin_marker_button.classList.add("current-marker");
 set_thin_marker_button.textContent = "thin";
 
 const set_thick_marker_button = document.createElement("button");
@@ -69,10 +69,24 @@ const current_line: LineCommand = {
 
 const lines: LineCommand[] = [];
 const redo_stack: LineCommand[] = [];
-let is_pen_active: ActivePen = false;
+
+const pen: CursorCommand = {
+  active: false,
+  x: 0,
+  y: 0,
+  sticker: false,
+  execute: function (ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    if (!this.sticker) {
+      ctx.arc(this?.x, this?.y, ctx.lineWidth, 0, 2 * Math.PI);
+    }
+    ctx.stroke();
+  },
+};
+
 const ctx = canvas.getContext("2d");
-const thin_line_width = 2;
-const thick_line_width = 5;
+const thin_line_width = 1.5;
+const thick_line_width = 4;
 ctx!.fillStyle = "white";
 ctx!.lineWidth = thin_line_width;
 
@@ -82,24 +96,59 @@ const set_marker_width: MarkerCommand = (thin: boolean) => {
 
 const start_line_handler = () => {
   current_line.thickness = ctx?.lineWidth;
-  is_pen_active = true;
+  pen.active = true;
 };
 
 const finish_line_handler = () => {
-  if (!is_pen_active) return;
-  lines.push({ ...current_line });
-  current_line.points = [];
-  is_pen_active = false;
+  if (!pen.active) {
+    pen.x = NaN
+    pen.y = NaN
+  } else {
+    lines.push({ ...current_line});
+    current_line.points = [];
+    pen.active = false;
+  }
+  canvas.dispatchEvent(new Event("drawing-changed"));
+};
+
+const handle_mouse_move = (e: MouseEvent) => {
+  if (!pen.active) {
+    canvas.dispatchEvent(new Event("tool-moved"));
+  }
+  pen.x = e.offsetX;
+  pen.y = e.offsetY;
+  log_point(e);
+};
+
+const handle_thin_marker_toggle = () => {
+  set_thick_marker_button.classList.remove("current-marker");
+  set_thin_marker_button.classList.add("current-marker");
+  set_marker_width(true);
+};
+
+const handle_thick_marker_toggle = () => {
+  set_thin_marker_button.classList.remove("current-marker");
+  set_thick_marker_button.classList.add("current-marker");
+  set_marker_width(false);
+};
+
+const handle_undo_redo: UndoRedoCommand = (undo: boolean) => {
+  if (undo && lines.length === 0) return;
+  if (!undo && redo_stack.length === 0) return;
+  if (undo) redo_stack.push(lines.pop() as LineCommand);
+  else lines.push(redo_stack.pop() as LineCommand);
+  canvas.dispatchEvent(new Event("drawing-changed"));
 };
 
 const log_point = (e: MouseEvent) => {
-  if (!is_pen_active) return;
+  if (!pen.active) return;
   current_line.grow(e.offsetX, e.offsetY);
   canvas.dispatchEvent(new Event("drawing-changed"));
 };
 
 const draw = (ctx: CanvasRenderingContext2D) => {
   ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  pen.execute(ctx);
   current_line.execute();
   lines.forEach((e) => e.execute());
 };
@@ -111,36 +160,24 @@ const clear_canvas = () => {
   redo_stack.length = 0;
 };
 
-const handle_undo_redo: UndoRedoCommand = (undo: boolean) => {
-  if (undo && lines.length === 0) return;
-  if (!undo && redo_stack.length === 0) return;
-  if (undo) redo_stack.push(lines.pop() as LineCommand);
-  else lines.push(redo_stack.pop() as LineCommand);
-  canvas.dispatchEvent(new Event("drawing-changed"));
-};
-
+/** REGISTER EVENTS TO THE CANVAS */
 canvas.addEventListener("mousedown", start_line_handler);
 canvas.addEventListener("mouseup", finish_line_handler);
 canvas.addEventListener("mouseleave", finish_line_handler);
-canvas.addEventListener("mousemove", (e) => log_point(e));
+canvas.addEventListener("mousemove", (e) => {
+  handle_mouse_move(e);
+});
 canvas.addEventListener(
   "drawing-changed",
   () => draw(ctx as CanvasRenderingContext2D),
 );
+canvas.addEventListener("tool-moved",
+  () => draw(ctx as CanvasRenderingContext2D)
+);
 
+/** REGISTER EVENTS FOR BUTTONS */
 clear_button.addEventListener("click", clear_canvas);
 undo_button.addEventListener("click", () => handle_undo_redo(true));
 redo_button.addEventListener("click", () => handle_undo_redo(false));
-set_thin_marker_button.addEventListener("click", () => {
-  set_thick_marker_button.classList.remove("current-marker");
-  set_thin_marker_button.classList.add("current-marker");
-  set_marker_width(true);
-});
-set_thick_marker_button.addEventListener(
-  "click",
-  () => {
-    set_thin_marker_button.classList.remove("current-marker");
-    set_thick_marker_button.classList.add("current-marker");
-    set_marker_width(false);
-  },
-);
+set_thin_marker_button.addEventListener("click", handle_thin_marker_toggle);
+set_thick_marker_button.addEventListener("click", handle_thick_marker_toggle);
